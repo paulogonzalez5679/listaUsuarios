146fb7e7d05a69547adfc27e7ba78d17c202d9f7
paulogonzalez5679 <paulomga10@gmail.com>
2022-10-25 10:23:51 -0500

    login ok

--- usuarios/src/app/app-routing.module.ts
+++ usuarios/src/app/app-routing.module.ts
@@ -4,8 +4,13 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
 ];
 @NgModule({
   imports: [
